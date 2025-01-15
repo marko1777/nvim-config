8e33f5b3b126179fd034089484c22c56ab4607da
Mark Puha <p.mark95@gmail.com>
2025-01-15 12:15:57 +0100

    feat: disable tressiter for large file

--- lua/mark/lazy/treesitter.lua
+++ lua/mark/lazy/treesitter.lua
@@ -10,6 +10,7 @@ return {
 			multiline_threshold = 1,
 		})
 		require("nvim-treesitter.configs").setup({
+			modules = {},
 			-- A list of parser names, or "all"
 			ensure_installed = {
 				"vimdoc",
@@ -23,6 +24,7 @@ return {
 				"jsdoc",
 				"bash",
 			},
+			ignore_install = { "json" },
 
 			-- Install parsers synchronously (only applied to `ensure_installed`)
 			sync_install = false,
@@ -35,15 +37,27 @@ return {
 				enable = true,
 			},
 
+			additional_vim_regex_highlighting = { "markdown" },
+
 			highlight = {
 				-- `false` will disable the whole extension
 				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = { "markdown" },
+				disable = function(lang, buf)
+					if lang == "json" then
+						print("disabled")
+						return true
+					end
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, size = pcall(vim.fn.getfsize, vim.api.nvim_buf_get_name(buf))
+					if ok and size and size > max_filesize then
+						vim.notify(
+							"File larger than 100KB treesitter disabled for performance",
+							vim.log.levels.WARN,
+							{ title = "Treesitter" }
+						)
+						return true
+					end
+				end,
 			},
 		})
 
