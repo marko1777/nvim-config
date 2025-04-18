26b80639e368bf0bf3312dd6566c96a0414a8c63
Mark Puha <p.mark95@gmail.com>
2025-04-18 11:48:04 +0200

    chore: remove  from quickfixlist

--- lua/mark/init.lua
+++ lua/mark/init.lua
@@ -45,7 +45,7 @@ autocmd({ "BufWritePre" }, {
 	command = [[%s/\s\+$//e]],
 })
 
-vim.api.nvim_create_autocmd("FileType", {
+autocmd("FileType", {
 	pattern = "netrw", -- Only apply to Netrw buffers
 	callback = function()
 		local bind = function(lhs, rhs)
@@ -56,29 +56,6 @@ vim.api.nvim_create_autocmd("FileType", {
 		bind("<C-l>", "<C-w>l")
 	end,
 })
-
--- vim.api.nvim_create_autocmd("BufWriteCmd", {
---     group = vim.api.nvim_create_augroup("GoFormat", { clear = true }),
---     pattern = "*.go",
---     callback = function()
---         local current_file = vim.fn.expand("%:p") -- Get the full file path
---         local formatted_code = vim.fn.system(string.format("goimports %s | golines --max-len=80", current_file))
---         if formatted_code ~= nil then -- If there were no errors (formatted_code is not nil)
---           vim.api.nvim_buf_set_lines(0, 0, -1, -1, vim.split(formatted_code, "\n")) -- Replace buffer content
---           vim.cmd("write") -- Save the file
---         end
---     end
--- })
-
--- autocmd("BufWritePre", {
---   pattern = { "*.go" },  -- Only for Go find_files
---   command = ":w !goimports % | golines --max-len=80 > %", -- Combine both commands
---   -- callback = function()
---   --   vim.lsp.buf.format { async = false, filter = function(client) return client.name == "gopls" end }
---   --   vim.cmd("execute '!golines --max-len=80'")
---   -- end,
---   group = vim.api.nvim_create_augroup("GoImportsOnSave", { clear = true }), -- Group for easier management
--- })
 
 autocmd("LspAttach", {
 	group = MarkGroup,
@@ -106,7 +83,9 @@ autocmd("LspAttach", {
 		vim.keymap.set("n", "<leader>vrn", function()
 			vim.lsp.buf.rename()
 		end, opts)
-		vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
 		vim.keymap.set("n", "[d", function()
 			vim.diagnostic.jump({ count = 1, float = true })
 		end, opts)
@@ -156,4 +135,78 @@ vim.api.nvim_set_hl(0, "CurSearch", { bg = "#FFA500", fg = "black", bold = true,
 --   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
 -- end, { silent = true })
 --
---
+local del_qf_item = function()
+	local items = vim.fn.getqflist()
+	local line = vim.fn.line(".")
+	table.remove(items, line)
+	vim.fn.setqflist(items, "r")
+	vim.api.nvim_win_set_cursor(0, { line, 0 })
+end
+
+-- this is super basic. I might improve it at some point, but it's good enough for how little I use
+-- the QF list
+-- vim.keymap.set("n", "dd", del_qf_item, { silent = true, buffer = true, desc = "Remove entry from QF" })
+-- vim.keymap.set("v", "D", del_qf_item, { silent = true, buffer = true, desc = "Remove entry from QF" })
+
+-- Remove items from quickfix list.
+-- `dd` to delete in Normal
+-- `d` to delete Visual selection
+local function delete_qf_items()
+	local mode = vim.api.nvim_get_mode()["mode"]
+
+	local start_idx
+	local count
+
+	if mode == "n" then
+		-- Normal mode
+		start_idx = vim.fn.line(".")
+		count = vim.v.count > 0 and vim.v.count or 1
+	else
+		-- Visual mode
+		local v_start_idx = vim.fn.line("v")
+		local v_end_idx = vim.fn.line(".")
+
+		start_idx = math.min(v_start_idx, v_end_idx)
+		count = math.abs(v_end_idx - v_start_idx) + 1
+
+		-- Go back to normal
+		vim.api.nvim_feedkeys(
+			vim.api.nvim_replace_termcodes(
+				"<esc>", -- what to escape
+				true, -- Vim leftovers
+				false, -- Also replace `<lt>`?
+				true -- Replace keycodes (like `<esc>`)?
+			),
+			"x", -- Mode flag
+			false -- Should be false, since we already `nvim_replace_termcodes()`
+		)
+	end
+
+	local qflist = vim.fn.getqflist()
+
+	for _ = 1, count, 1 do
+		table.remove(qflist, start_idx)
+	end
+
+	vim.fn.setqflist(qflist, "r")
+	vim.fn.cursor(start_idx, 1)
+end
+
+local custom_group = vim.api.nvim_create_augroup("custom", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = custom_group,
+	pattern = "qf",
+	callback = function()
+		-- Do not show quickfix in buffer lists.
+		vim.api.nvim_buf_set_option(0, "buflisted", false)
+
+		-- -- Escape closes quickfix window.
+		-- vim.keymap.set("n", "<ESC>", "<CMD>cclose<CR>", { buffer = true, remap = false, silent = true })
+
+		-- `dd` deletes an item from the list.
+		vim.keymap.set("n", "dd", delete_qf_items, { buffer = true })
+		vim.keymap.set("x", "d", delete_qf_items, { buffer = true })
+	end,
+	desc = "Quickfix tweaks",
+})
