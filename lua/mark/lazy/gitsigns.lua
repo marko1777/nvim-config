d041acf2254387a73f8c83a621a67b5ea7d1d7bd
Mark Puha <p.mark95@gmail.com>
2025-04-18 11:39:23 +0200

    gitsigns lazy load

--- lua/mark/lazy/gitsigns.lua
+++ lua/mark/lazy/gitsigns.lua
@@ -1,10 +1,13 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	-- enabled = false,
+	keys = {
+		vim.keymap.set("n", "<leader>gb", ":Gitsigns blame<CR>"),
+		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>"),
+		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>"),
+	},
 	config = function()
 		require("gitsigns").setup({ signcolumn = false })
 
-		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
-		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>")
 	end,
 }
